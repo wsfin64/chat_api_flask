f659b6a46c31ff6c2e40d0f77182534a696e3b70
Wellington <wsfin64@gmail.com>
2022-08-14 15:29:37 -0300

    favorites

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def root():
     return "Please, inform model's username in path request"
 
+
 @app.get('/<model_name>')
 def get_model(model_name):  # put application's code here
 
@@ -31,12 +32,37 @@ def get_model(model_name):  # put application's code here
     else:
         raise Exception("There is a problem in your request")
 
+
+@app.get("/favorites")
+def verify_if_favorites_is_online():
+
+    favorites_online = []
+    #favorites = list(environ.get('FAVORITES'))
+    favorites = environ.get('FAVORITES').split(',')
+
+    for favorite in favorites:
+        fav = favorite.replace(" ", "").replace("'", "")
+        req = requests.get(f"{environ.get('CHAT_URL')}/{fav}")
+
+        if req.status_code == 200:
+            model = ModelSchema().load(req.json())
+
+            if model.room_status == 'public':
+                favorites_online.append(model)
+
+    if len(favorites_online) == 0:
+        return jsonify("None of your favorites are online now"), 200
+
+    return jsonify(favorites_online), 200
+
+
 @app.post("/<model_name>")
 def post_model(model_name):
     return f"""
         <h2>POST method not allowed, use GET instead</h2>
         <img src={environ.get('GIF_IMAGE')} alt='gia_baker'>
     """
+
 
 if __name__ == '__main__':
     app.run()

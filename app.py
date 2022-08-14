e20580795c38d9ae862dc8961cbfc1c439581ed3
Wellington <wsfin64@gmail.com>
2022-08-14 12:02:34 -0300

    returning 404 error

--- app.py
+++ app.py
@@ -2,19 +2,35 @@ import requests
 from flask import Flask, jsonify
 from os import environ
 from model.model import ModelSchema
+from exceptions.custom_exceptions import MissingModelException
 
 app = Flask(__name__)
+
+
+@app.get("/")
+def root():
+    return "Please, inform model username in path request"
 
 
 @app.get('/<model_name>')
 def get_model(model_name):  # put application's code here
 
+    if model_name is None:
+        raise MissingModelException("Please, inform model username in path request")
+
     req = requests.get(f"{environ.get('CHAT_URL')}/{model_name}")
 
-    modelo = ModelSchema().load(req.json())
+    if req.status_code == 200:
 
-    return jsonify(modelo.to_json())
+        modelo = ModelSchema().load(req.json())
 
+        return jsonify(modelo.to_json()), 200
+
+    elif req.status_code == 404:
+        return jsonify({"error": f"Model {model_name} not found"}), 404
+
+    else:
+        raise Exception("There is a problem in your request")
 
 if __name__ == '__main__':
     app.run()
